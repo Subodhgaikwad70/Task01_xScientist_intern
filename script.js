cb3b2ff4346ad3440ea8e6caec9aceb858e2055b
subodh <subodhgaikwad70@gmail.com>
2023-09-02 01:35:12 +0530

    data fetched through api and card design changed

---
+++ script.js
@@ -0,0 +1,44 @@
+// const username_field = document.getElementById("username");
+const image_field = document.getElementById("photo");
+const name_field = document.getElementById("name");
+const gender_field = document.getElementById("gender");
+const dob_field = document.getElementById("dob");
+const address_field = document.getElementById("address");
+const email_field = document.getElementById("email");
+
+function reload(){
+
+    
+    fetch('https://randomuser.me/api/').then((data)=>{
+        return data.json()
+    }).then((data)=>{
+    
+        var username, image, fname, gender, dob, address, email;
+    
+        data = data.results[0];
+        console.log(data);
+        username = data.login.username;
+        image = data.picture.large;
+        fname = data.name.first+" "+data.name.last;
+        gender = data.gender;
+        dob = data.dob.date;
+        address = data.location;
+        address = address.street.number+", "+address.street.name+", "+address.city+", "+address.state+", "+address.country+", "+address.postcode;
+        email = data.email;
+    
+        var date = new Date(dob);
+        date = date.toLocaleDateString();
+    
+    
+        // username_field.innerText = username;
+        image_field.src = image;
+        address_field.innerText = address;
+        name_field.innerText = fname;
+        gender_field.innerText = gender;
+        dob_field.innerText = date;
+        email_field.innerText = email;
+    
+    })
+}
+
+reload();
